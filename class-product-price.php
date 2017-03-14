6bcc774ac3e55f375c6ebb608b9001cc7f921d84
Rune Hallonen Abrahamsen <rune@hallonen.com>
2017-03-14 17:57:58 +0100

    Working copy as set by the test parameters in the README.md

--- class-product-price.php
+++ class-product-price.php
@@ -1,48 +1,95 @@
 <?php
-error_reporting( 0) ;
-ini_set('display_errors', 0)
+error_reporting(0);
+ini_set('display_errors', 0);
 
-// Some class
-class product_Price
-
-  {
-    public $Name;
-		public $currency;
-    public $taxP = 25; // tax in percentage
-    public $taxA;
-	public $fees = [];
-  function  product_Price(  ){
-		list( $args[],$args[],$args[] ) = func_get_args();
-    $this->Name = $args[0];
-    $this->currency = $args[1];
-	$this->p = $args[2];
+/**
+ * Class to handle products
+ *
+ * Set and get the various fields of the products
+ */
+class ProductPrice
+{
+  public $name; // product name
+	public $currency; // product currency
+	public $p; // product price
+  public $taxP = 25; // tax in percentage
+  public $taxA; // tax in amount
+	public $fees = array('11338' => 25); // array of productcodes with fees
+	
+	/**
+     * Construct the class with basic data
+    */
+  function __construct(){
+		list( $p, $currency, $name ) = func_get_args();
+    $this->name = $name;
+    $this->currency = $currency;
+		$p = $this->product_fees($p); // add any fees to price
+		$this->set_price($p); // set the prices
   }
+
   /**
-   * [setPrice description]
-   * @param float $p [description]
+   * Sets the price for the product
+   * 
+   * @param float $p  The price of the product
+   * @param float $pp The VAT percentage (default is 25)
    */
-  function setPrice($p, $pp = 25  ) {
+  function set_price($p, $pp = 25  ) {
     $this->taxP = $pp;
-    $this->taxA = $p / 125 * 100;
+    $this->taxA = $p * ($pp / 100);
     $this->p = $p;
   }
-  function GetPrice($j=1) {
-    $which = ereg( '/\-([.*])/', $this->name, $match); $f = FALSE;
 
-    if ( $match[1] == '11338' ) {
-      $this->p += 25;
-    }
-
-
-    return ($this->p*$j)."\n";
+	/**
+     * Checks if fees should be added to the price
+     *
+     * Extracts the numbers from the name of the product
+     * and checks if there are any fees for that product
+     * 
+     * @param float $p The product price
+     *
+     * @return float $p the new price
+    */
+  function product_fees($p) {
+    $which = preg_match( '([\d]+)', $this->name, $match); // extract the numbers from the productname
+		
+    if (array_key_exists($match[0], $this->fees ))
+			return $p + $this->fees[$match[0]]; // fees added to the price
+		else
+			return $p;
   }
 
-  function getPriceExcl_tax($j) {
-    return $this->p - $this->taxA * $j;
+	/**
+     * Get the price excluding VAT
+     *
+     * @param float $j quantity of the product
+     *
+     * @return float price without VAT
+    */
+  function get_price_excl_tax($j) {
+    return $this->p * $j;
   }
-  function htm_pric($qty = 1, $method = 'GetPrice' ){
-    $🌎 = '<div  id="price”>%d</div>';
-    printf($🌎."\n",call_user_method_array($method,$this,[$qty]));
+	
+	/**
+     * Get the price including VAT
+     *
+     * @param float $j quantity of the product
+     *
+     * @return float price with VAT
+    */
+	function get_price_incl_tax($j) {
+    return ($this->p + $this->taxA) * $j;
+  }
+
+	/**
+     * Get the price in html format
+     *
+     * @param float  $qty quantity of products
+     * @param string $method name of the method of VAT calculation
+    */
+  function get_htm_price($qty = 1, $method){
+		$htm = '<div id="price">%d</div>';
+		printf($htm, call_user_func_array(array($this, $method), [$qty]));
   }
 }
 
+?>
--- index.php
+++ index.php
@@ -17,22 +17,22 @@
   <section id="information"></section>
   <section id="code">
     <?php
-      $content = file_get_contents( 'class-product-price.php' );
       $prices = [];
-      if ( ! @eval( '?>'. $content ) ) {
+      
+      if ((include_once 'class-product-price.php') == FALSE) {
         echo( 'Could not include the product price class' );
       }
       else {
-        $prices[] = new product_Price( 200, 'EUR', 'SKU-11334' );
-        $prices[] = new product_Price( 250, 'USD', 'SKU-11334' );
-        $prices[] = new product_Price( 252, 'USD', 'SKU-11335' );
-        $prices[] = new product_Price( 550, 'USD', 'SKU-11336' );
-        $prices[] = new product_Price( 530, 'USD', 'SKU-11337' );
-        $prices[] = new product_Price( 133, 'USD', 'SKU-11338' );
-        $prices[] = new product_Price( 441, 'USD', 'SKU-11339' );
-        $prices[] = new product_Price( 867, 'USD', 'SKU-11341' );
+        $prices[] = new ProductPrice( 200, 'EUR', 'SKU-11334' );
+        $prices[] = new ProductPrice( 250, 'USD', 'SKU-11334' );
+        $prices[] = new ProductPrice( 252, 'USD', 'SKU-11335' );
+        $prices[] = new ProductPrice( 550, 'USD', 'SKU-11336' );
+        $prices[] = new ProductPrice( 530, 'USD', 'SKU-11337' );
+        $prices[] = new ProductPrice( 133, 'USD', 'SKU-11338' );
+        $prices[] = new ProductPrice( 441, 'USD', 'SKU-11339' );
+        $prices[] = new ProductPrice( 867, 'USD', 'SKU-11341' );
       }
-      $prices[3]->setPrice( 252, 15 );
+      $prices[3]->set_price( 252, 15 );
 
       $quantity = 55;
     ?>
@@ -54,16 +54,16 @@
       <tbody>
       <?php foreach ( $prices as $price ): $quantity = 5 + ( $quantity * $quantity ) % 33; ?>
         <tr>
-          <td><?= $price->Name; ?></td>
+          <td><?= $price->name; ?></td>
           <td><?= $price->currency; ?></td>
           <td><?= $price->taxP; ?></td>
           <td><?= $price->taxA; ?></td>
-          <td><?= $price->geT_htm_price(1, 'getPriceExcl_tax'); ?></td>
-          <td><?= $price->geT_htm_price(); ?></td>
+          <td><?= $price->get_htm_price(1, 'get_price_excl_tax'); ?></td>
+          <td><?= $price->get_htm_price(1, 'get_price_incl_tax'); ?></td>
           <td><?= $quantity; ?></td>
           <td><?= $price->taxA * $quantity; ?></td>
-          <td><?= $price->geT_htm_price($quantity, 'getPriceExcl_tax'); ?></td>
-          <td><?= $price->geT_htm_price($quantity); ?></td>
+          <td><?= $price->get_htm_price($quantity, 'get_price_excl_tax'); ?></td>
+          <td><?= $price->get_htm_price($quantity, 'get_price_incl_tax'); ?></td>
         </tr>
       <?php endforeach; ?>
       </tbody>
